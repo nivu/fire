83ab36d33fd92c070d369d6f0528ddd09f75f6b7
Navaneeth Malingan <navneetnivu07@gmail.com>
2019-06-15 21:37:34 +0530

    firestore addede

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'fire';
+  title = 'jinu';
+
+  val: any = 1;
+
+  items: Observable<any[]>;
+  constructor(db: AngularFireDatabase) {
+    db.list('buttons').valueChanges().subscribe(data => {
+      console.log(data);
+      this.val = data;
+    });
+
+    db.list('buttons2').valueChanges().subscribe(data => {
+      console.log(data);
+      this.val = data;
+    });
+
+    db.list('buttons3').valueChanges().subscribe(data => {
+      console.log(data);
+      this.val = data;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import {
+  AngularFirestore
+} from '@angular/fire/firestore';
+
+import { AngularFireDatabase } from '@angular/fire/database';
+
+
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [AngularFirestore, AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
